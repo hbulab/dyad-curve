18367e89eb0102b113686fc3f5f32223a9d72c3e
Adrien Gregorj <adrien.gregorj@gmail.com>
2025-03-21 17:01:57 +0900

    update requirements

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create the data structure
+mkdir data
+mkdir data/intermediate
+mkdir -p data/figures/02_04_meta_trajectories/06
+mkdir -p data/figures/02_04_meta_trajectories/08
+mkdir -p data/figures/02_05_meta_trajectories_velocities_wrt_size/06
+mkdir -p data/figures/02_05_meta_trajectories_velocities_wrt_size/08
+mkdir -p data/figures/02_05_meta_trajectories_wrt_size/06
+mkdir -p data/figures/02_05_meta_trajectories_wrt_size/08
+mkdir -p data/figures/02_06_meta_trajectories_wrt_size/06
+mkdir -p data/figures/02_06_meta_trajectories_wrt_size/08
+mkdir -p data/figures/02_06_meta_trajectories_wrt_interaction/06
+mkdir -p data/figures/02_06_meta_trajectories_wrt_interaction/08
+mkdir -p data/figures/02_07_meta_trajectories
+mkdir -p data/figures/02_08_trajectories_with_velocity_curvature
+mkdir -p data/figures/02_08_velocity_curvature
+mkdir -p data/figures/02_09_meta_trajectories_inner_outer
+mkdir -p data/figures/02_10_trajectories_inner_outer_faster
+mkdir -p data/figures/02_10_trajectories_with_velocity_curvature_inner_outer
+mkdir -p data/figures/02_10_velocity_curvature_inner_outer
+mkdir -p data/figures/02_12_osculating_circles
+mkdir -p data/figures/03_03_meta_trajectories_wrt_size_atc
+mkdir -p data/figures/03_04_meta_trajectories_wrt_social_relation
+mkdir -p data/figures/test_angular_velocity
+
+mkdir data/tables
